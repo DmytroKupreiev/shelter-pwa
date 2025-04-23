88666d65b1e26070ad83f91ead0bb6b85516e0fa
DmytroKupreiev <Hivahova43@gmail.com>
2025-04-23 17:29:38 +0000

    Creating home page & providing HttpClient and Observable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { AnimalService, Animal } from '../../service/animal.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['home.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements AfterViewInit, OnInit {
+  constructor(private animalService: AnimalService) {}
+
+  animals: Animal[] = [];
+  randomAnimals: Animal[] = [];
+
+  ngOnInit(): void {
+    this.animalService.getAnimals().subscribe({
+      next: (data) => this.animals = data,
+      error: (err) => console.error('Error loading animals', err)
+    });
+    this.animalService.getRandomAnimals(1).subscribe({
+      next: (data) => this.randomAnimals = data,
+      error: (err) => console.error('Error loading random animals', err)
+    });
+  }
+
+  ngAfterViewInit(): void {
+    if ((window as any).FinisherHeader) {
+      new (window as any).FinisherHeader({
+        count: 100,
+        size: { min: 1, max: 2, pulse: 0 },
+        speed: {
+          x: { min: 0, max: 0.4 },
+          y: { min: 0, max: 0.6 }
+        },
+        colors: {
+          background: "#1e302b",
+          particles: ["#fbfcca", "#d7f3fe", "#ffd0a7"]
+        },
+        blending: "none",
+        opacity: {
+          center: 1,
+          edge: 0.5
+        },
+        skew: -2,
+        shapes: ["c"]
+      });
+    } else {
+      console.error('FinisherHeader not loaded');
+    }
+  }
+  
+  navigateToAnimals() {
+
+  }
+
+  navigateToContact() {
+
+  }
 }
