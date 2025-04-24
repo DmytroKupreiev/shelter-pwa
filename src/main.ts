bc60aeaec459a11f1f03fed50f9fd1ea58e1d055
DmytroKupreiev <Hivahova43@gmail.com>
2025-04-24 10:57:48 +0000

    Adding ionic storage

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,23 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { provideHttpClient } from '@angular/common/http';
 import { provideServiceWorker } from '@angular/service-worker';
 import { isDevMode } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
+import { Drivers } from '@ionic/storage';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';  
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
+    importProvidersFrom(
+      IonicStorageModule.forRoot({
+        name: '__mydb',
+        driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+      })
+    ),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
     provideServiceWorker('ngsw-worker.js', {
@@ -19,4 +28,4 @@ bootstrapApplication(AppComponent, {
       registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
-});
+}).catch(err => console.error(err));
